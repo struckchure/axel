5953e66a6ba148d5d1479285e00d6ff094757d3b
Mohammed Al-Ameen <ameenmohammed2311@gmail.com>
2026-04-18 22:28:26 +0100

    ci: updates ci config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
+  base: "struckchure.github.io/axel/",
   title: "Axel",
   description: "Schema and query language tool for PostgreSQL",
 
